fe06d7fd6d029b3ecac4f128704718296620106d
gn5r <shangyuan.tuolang@gmail.com>
2018-06-01 15:11:18 +0900

    JSONObjectライブラリ使用。postメソッド・createSessionsメソッドの修正

--- README.md
+++ README.md
@@ -3,5 +3,16 @@
 ## 更新履歴
 
 - 2018年5月30日(水) initial commit
-
+- 2018年6月1日(金) JSONObjectライブラリを利用し、jsonパース。postメソッド・createSessionsメソッドの修正
 ----
+
+## 当ライブラリの使い方
+Boccoとやり取りするために **APIKey** を申請しておきます。<br>
+また、スマートフォンアプリから初期設定を済ませておいてください。
+
+### BoccoAPI(APIKey,Email,Password)
+BoccoAPIクラスのコンストラクタには受け取ったAPIKey、アプリで設定したメールアドレスとパスワードを入力
+
+### public boolean createSessions()
+コンストラクタで設定したパラメータを使い、セッションを作成しアクセストークンを取得します。<br>
+問題なくトークンを取得できると **true** を返し、パラメータのミス等があった場合 **false** を返します。
--- src/main/java/hal/toyko/rd4c/bocco4j/BoccoAPI.java
+++ src/main/java/hal/toyko/rd4c/bocco4j/BoccoAPI.java
@@ -11,6 +11,7 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import org.json.JSONObject;
 
 /**
  *
@@ -18,10 +19,10 @@ import java.net.URL;
  */
 public class BoccoAPI {
 
-    private String APIKey;
-    private String Email;
-    private String Password;
-    private String APIEndpoint = "https://api.bocco.me/alpha/";
+    private final String APIKey;
+    private final String Email;
+    private final String Password;
+    private final String APIEndpoint = "https://api.bocco.me/alpha/";
 
     private String accsessToken;
 
@@ -31,18 +32,60 @@ public class BoccoAPI {
         this.Password = Password;
     }
 
-    public boolean createSessions() {
-        String data = "apikey = " + this.APIKey + "&email=" + this.Email + "&password=" + this.Password;
+    /*    getリクエスト    */
+    private String get(String URL, String data) {
+        HttpURLConnection connection = null;
+        StringBuilder result = new StringBuilder();
 
-        String json = this.post("sessions", data, "application/x-www-form-urlencoded");
+        try {
+            URL url = new URL(this.APIEndpoint + URL + data);
 
-        System.out.println(json);
-        if(json.length() == 0 ) return false;
-        
-        return true;
+            connection = (HttpURLConnection) url.openConnection();
+            connection.setDoOutput(true);
+            connection.setRequestMethod("GET");
+            connection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+            connection.connect();
+
+            // HTTPレスポンスコード
+            final int status = connection.getResponseCode();
+            if (status == HttpURLConnection.HTTP_OK) {
+                // 通信に成功した
+                // テキストを取得する
+                final InputStream in = connection.getInputStream();
+                String encoding = connection.getContentEncoding();
+                if (null == encoding) {
+                    encoding = "UTF-8";
+                }
+                final InputStreamReader inReader = new InputStreamReader(in, encoding);
+                final BufferedReader bufReader = new BufferedReader(inReader);
+                String line = null;
+                // 1行ずつテキストを読み込む
+                while ((line = bufReader.readLine()) != null) {
+                    result.append(line);
+                }
+                bufReader.close();
+                inReader.close();
+                in.close();
+            } else {
+                System.out.println(status);
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (connection != null) {
+                // コネクションを切断
+                connection.disconnect();
+            }
+        }
+        System.out.println("result=" + result.toString());
+
+        return result.toString();
+
     }
 
-    public String post(String URL, String data, String ContentType) {
+    /*    postリクエスト  */
+    private String post(String URL, String data) {
 
         HttpURLConnection connection = null;
         StringBuilder result = new StringBuilder();
@@ -53,7 +96,7 @@ public class BoccoAPI {
             connection = (HttpURLConnection) url.openConnection();
             connection.setDoOutput(true);
             connection.setRequestMethod("POST");
-            connection.setRequestProperty("Content-Type", ContentType);
+            connection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
 
             OutputStreamWriter out = new OutputStreamWriter(connection.getOutputStream());
             out.write(data);
@@ -86,6 +129,7 @@ public class BoccoAPI {
             }
 
         } catch (Exception e) {
+            e.printStackTrace();
         } finally {
             if (connection != null) {
                 // コネクションを切断
@@ -96,7 +140,32 @@ public class BoccoAPI {
         return result.toString();
     }
 
-    public String getAccsessToken() {
+    /*    セッションを作成しアクセストークンを取得する*/
+    public boolean createSessions() {
+        String data = "apikey = " + this.APIKey + "&email=" + this.Email + "&password=" + this.Password;
+
+        String response = this.post("sessions/", data);
+
+        JSONObject json = new JSONObject(response);
+
+        System.out.println(json.getString("access_token"));
+
+        if (json.getString("code").matches("401001")) {
+            return false;
+        }
+
+        return true;
+    }
+
+    /*/
+    
+    /*    ルームへメッセージを送信    */
+    public boolean postMessage(String message) {
+
+        return true;
+    }
+
+    private String getAccsessToken() {
         return accsessToken;
     }
 
