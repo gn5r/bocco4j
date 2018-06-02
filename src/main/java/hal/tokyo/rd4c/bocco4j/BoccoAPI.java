42298845be3ecc32ec50a816e4bc537a929ae083
gn5r <shangyuan.tuolang@gmail.com>
2018-06-03 08:59:43 +0900

    メッセージ送信が可能に

--- src/main/java/hal/tokyo/rd4c/bocco4j/BoccoAPI.java
+++ src/main/java/hal/tokyo/rd4c/bocco4j/BoccoAPI.java
@@ -10,6 +10,7 @@ import java.io.InputStreamReader;
 import java.io.PrintStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.UUID;
 import org.json.JSONObject;
 
 /**
@@ -23,8 +24,12 @@ public class BoccoAPI {
     private final String Password;
     private final String Host = "https://api.bocco.me";
     private final String Session = "/alpha/sessions";
+    private final String Rooms = "/alpha/rooms";
+    private final String Rooms_Joined = "/joined?";
+    private final String Message = "/messages";
 
     private String accsessToken;
+    private String roomID;
 
     public BoccoAPI(String APIKey, String Email, String Password) {
         this.APIKey = APIKey;
@@ -33,8 +38,9 @@ public class BoccoAPI {
     }
 
     /*    POSTリクエスト    */
-    public String post(String url, String data) throws Exception {
-        URL urls = new URL(url);
+    private String post(String url, String data) throws Exception {
+        /*    接続先URLのインスタンス生成    */
+        URL urls = new URL(Host + url);
         HttpURLConnection con = (HttpURLConnection) urls.openConnection();
         StringBuilder result = new StringBuilder();
 
@@ -46,11 +52,13 @@ public class BoccoAPI {
         con.setRequestMethod("POST");
         /*    Content-Typeを設定    */
         con.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+        con.setRequestProperty("Accept-Language", "jp");
 
         /*    リクエストボディの作成    */
         PrintStream ps = new PrintStream(con.getOutputStream());
         ps.print(data);
         ps.close();
+        /*    接続開始    */
         con.connect();
 
         /*    HTTP通信の確認    */
@@ -69,7 +77,7 @@ public class BoccoAPI {
             reader.close();
 
         } else {
-            System.out.println("ERROR" + String.valueOf(statusCode));
+            System.out.println("ERROR Code:" + String.valueOf(statusCode));
         }
 
         con.disconnect();
@@ -83,20 +91,101 @@ public class BoccoAPI {
         String data = "apikey=" + APIKey + "&email=" + Email + "&password=" + Password;
 
         /*    JSONデータ取得    */
-        String json = post(Host + Session, data);
+        String json = post(Session, data);
 
-        /*    nullチェック    */
         if (json.matches("")) {
             return false;
         }
+
         /*    JSONパース    */
         JSONObject jsonObj = new JSONObject(json);
         /*    パースした内容からaccess_tokenを取り出す    */
         accsessToken = jsonObj.getString("access_token");
-        
+
         System.out.println("---- access_token ----");
         System.out.println(accsessToken);
-        
+
+        return true;
+    }
+
+    /*    GETリクエスト    */
+    private String get(String url, String data) throws Exception {
+        /*    接続先URLのインスタンス生成    */
+        URL urls = new URL(Host + url + data);
+        HttpURLConnection con = (HttpURLConnection) urls.openConnection();
+        StringBuilder result = new StringBuilder();
+
+        /*    レスポンスボディ受信を許可    */
+        con.setDoOutput(true);
+        /*    HTTPリクエストをGETに設定    */
+        con.setRequestMethod("GET");
+        /*    Content-Typeを設定    */
+        con.setRequestProperty("Content-Type", "application/json");
+
+        /*    接続開始    */
+        con.connect();
+
+        /*    HTTP通信の確認    */
+        final int statusCode = con.getResponseCode();
+
+        if (statusCode == HttpURLConnection.HTTP_OK) {
+
+            BufferedReader reader = new BufferedReader(new InputStreamReader(
+                    con.getInputStream()));
+            String body = "";
+
+            /*    レスポンスボディの情報を1行ずつ読み取る    */
+            while ((body = reader.readLine()) != null) {
+                result.append(body);
+            }
+            reader.close();
+
+        } else {
+            System.out.println("ERROR Code:" + String.valueOf(statusCode));
+        }
+
+        con.disconnect();
+
+        return result.toString();
+    }
+
+    public boolean getFirstRooID() throws Exception {
+        String data = "access_token=" + accsessToken;
+
+        String json = get(Rooms + Rooms_Joined, data);
+
+        if (json.matches("")) {
+            return false;
+        }
+
+        String room = json.substring(json.indexOf("[{\"uuid\":\"") + 1);
+        room = room.substring(0, room.indexOf("\"background_image\"") - 1) + "}\n";
+
+        /*    JSONパース    */
+        JSONObject jsonObj = new JSONObject(room);
+        roomID = jsonObj.getString("uuid");
+
+        System.out.println("---- roomID ----");
+        System.out.println(roomID);
+
+        return true;
+    }
+
+    public boolean postMessage(String message) throws Exception {
+        /*    UUID乱数ベースの作成*/
+        UUID uuid = UUID.randomUUID();
+
+        /*    POSTボディデータを作成    */
+        String data = "access_token=" + accsessToken
+                + "&media=text&text=" + message
+                + "&unique_id=" + uuid.toString();
+
+        String json = post(Rooms + "/" + roomID + Message, data);
+
+        if (json.matches("")) {
+            return false;
+        }
+
         return true;
     }
 }
--- src/main/java/hal/tokyo/rd4c/bocco4j/Sample.java
+++ src/main/java/hal/tokyo/rd4c/bocco4j/Sample.java
@@ -10,9 +10,18 @@ package hal.tokyo.rd4c.bocco4j;
  * @author gn5r
  */
 public class Sample {
-    public static void main(String[] args) throws Exception{
-        BoccoAPI boccoApi = new BoccoAPI("APIKey","exsample@bocco4j.co.jp","password");
-        
+
+    public static void main(String[] args) throws Exception {
+        BoccoAPI boccoApi = new BoccoAPI("oXclKHqmDQabjqfuLr2GNd0ASovTuzVyhAtvnyfDEbYGJD4hrIkmZ6hcUPqeP5CF",
+                "terashima.hidetoshi.150147@roivy.net",
+                "h8910teto");
+
         boccoApi.createSessions();
+        boccoApi.getFirstRooID();
+        if (boccoApi.postMessage(args[0]) == true) {
+            System.out.println("送信しました");
+        } else {
+            System.out.println("失敗です");
+        }
     }
 }
